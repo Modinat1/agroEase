452af3646517ca77ea5e9f1b0c16fd54542faa2f
susan <odiisusan@gmail.com>
2022-10-28 01:03:11 +0100

    contact-us-page_first commit

--- agroease-project/src/pages/Contact/Contact.js
+++ agroease-project/src/pages/Contact/Contact.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import ContactUs from './ContactUs'
 import Footer from '../../components/Footer/Footer'
 import Navbar1 from '../../components/Navbar1/Navbar1'
+import React from 'react'
 
 const Contact = () => {
   return (
     <div>
+
+      
         <Navbar1 />
 
+        <ContactUs/>
         <Footer />
     </div>
   )
---
+++ agroease-project/src/pages/Contact/ContactUs.jsx
@@ -0,0 +1,89 @@
+import './Contact.css'
+import { HiOutlineLocationMarker } from 'react-icons/hi'
+import { AiOutlinePhone } from 'react-icons/ai'
+import { AiOutlineMail } from 'react-icons/ai'
+
+// import contactEmail from '../../images/contactEmail.png'
+// import contactMap from '../../images/contactMap.png'
+// import contactPhone from '../../images/contactPhone.png'
+
+// import { BsLine } from "react-icons/bs"
+
+const ContactUs = () => {
+  // const style = { color: '#097c0d', fontSize: '5rem' }
+  return (
+    <div>
+      <section className="contact-container">
+        <h1 className="contact-heading"> Contact Us</h1>
+        <div className="contact-findUs">
+          <div className="contact-location">
+
+            <div className='contact-us-page-marker'>
+            <HiOutlineLocationMarker />
+            </div>
+            <h4 className=" Wuse-Zone-6"> Tech Specialist, Wuse Zone 6 </h4>
+          </div>
+          <div className="contact-number">
+            <div className="contact-us-page-phone">
+              <AiOutlinePhone />
+            </div>
+
+            <h4> 08082957336,</h4>
+            <h4> 08082957336</h4>
+          </div>
+          <div className="contact-email">
+            <div className="contact-us-email">
+              <AiOutlineMail />
+            </div>
+
+            <h4> agroease@gmail.com</h4>
+          </div>
+        </div>
+        {/* Form */}
+        <div className="form-container">
+          <form className="contact-form">
+            <h2 className="contactRequest-form"> Request Form </h2>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              id="name"
+              className="contact-page-input"
+              required
+            />
+            <input
+              type="email"
+              placeholder=" Email"
+              name="email"
+              id="email"
+              className="contact-page-input"
+              required
+            />
+            <input
+              type="tel"
+              placeholder="Phone"
+              name="tel"
+              id="tel"
+              className="contact-page-input"
+              required
+            />
+            <textarea
+              id="contact-textarea"
+              name="contact-textarea"
+              rows={10}
+              cols={65}
+              placeholder="Message..."
+              required
+              defaultValue={''}
+            />
+            <button className="contact-button" type="button">
+              Send Message
+            </button>
+          </form>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default ContactUs

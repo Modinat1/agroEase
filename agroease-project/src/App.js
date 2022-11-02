fc34738326bec2af38368abb7de47c045f473c24
godswill <godswillchibuzo@gmail.com>
2022-11-02 12:10:44 +0100

    the dashbord is ready

--- agroease-project/src/App.js
+++ agroease-project/src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
-import About from './pages/About/About';
+import Dashnav from './components/dashboard-nav-and-side/Dashnav';
+import Sidebar from './components/dashboard-nav-and-side/Sidebar';
+// import About from './pages/About/About';
 // import Blog from './pages/Blog/Blog';
 // import Contact from './pages/Contact/Contact';
 // import Help from './pages/Help/Help';
-import Home from './pages/Home/Home';
+// import Home from './pages/Home/Home';
+ 
 
 function App() {
   return (
     <div>
-      <About />
+      {/* <About /> */}
       {/* <Home /> */}
       {/* 
       <Contact />
       <Blog />
       <Help /> */}
       {/* <Account /> */}
+      <Sidebar/>
+      <Dashnav/>
+
     </div>
   );
 }

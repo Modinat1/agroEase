a91f0b6959740fb56396ca45e848e48574207b10
Odezulu Chukwuemeka <odezuluaugustine@gmail.com>
2022-11-16 13:28:00 +0100

    pulling the recent navbar

--- agroease-project/src/users/Farmer/FarmerProductUpload.js/FarmerAddProduct.js
+++ agroease-project/src/users/Farmer/FarmerProductUpload.js/FarmerAddProduct.js
@@ -1,13 +1,13 @@
 import React from "react";
-import BrokerDashnav from "../../../components/dashboard-nav-and-side/BrokerDashnav";
-import { FarmerSidebar } from "../../../components/dashboard-nav-and-side/FarmerSidebar";
+// import BrokerDashnav from "../../../components/dashboard-nav-and-side/BrokerDashnav";
+// import { FarmerSidebar } from "../../../components/dashboard-nav-and-side/FarmerSidebar";
 import { FarmerProductUpload } from "./FarmerProductUpload";
 
 export const FarmerAddProduct = () => {
 	return (
 		<>
-			<FarmerSidebar />
-			<BrokerDashnav />
+			{/* <FarmerSidebar /> */}
+			{/* <BrokerDashnav /> */}
 			<FarmerProductUpload />
 		</>
 	);

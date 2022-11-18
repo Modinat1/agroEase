d56988bb4b7406643db4aea9fb5e54d7db19f7e2
tiacode <nwacynti25@gmail.com>
2022-11-18 20:12:32 +0000

    Worked on the edit and delete logical function for the users

--- agroease-project/src/Context/user-context/UserType.js
+++ agroease-project/src/Context/user-context/UserType.js
@@ -1,2 +1,8 @@
 
-export const REGISTER_USER_FORM = "REGISTER_USER_FORM"
+export const ADD_USER = "ADD_USER"
+
+export const EDIT_USER = "EDIT_USER"
+
+export const DELETE_USER = "DELETE_USER"
+
+export const SUSPEND_USER = "SUSPEND_USER"

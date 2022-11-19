761336c305c3e0cbb27bf3e16ee9b6a220e9713f
sirconfy <csirconfy@gmail.com>
2022-11-19 06:28:56 +0100

    brokerDashoard form

--- agroease-project/src/users/Broker/BrokerProfile/IDGenerate.jsx
+++ agroease-project/src/users/Broker/BrokerProfile/IDGenerate.jsx
@@ -1,15 +1,20 @@
-import React from "react"
-import "./BrokersProfile.css"
+// import React from "react"
+import Swal from "sweetalert2";
+// import "./BrokersProfile.css"
 export const IdGenerator = () => {
-  return (
-   <div id="overlay">
-  <div className="displayID">
-    <h1>Congratulations</h1>
-    <h3>Your ID Number is</h3>
-    <h1 id="ID">12345</h1>
-      <button className="close" id="close">close</button>
-    </div>
-</div>
 
-  )
+  Swal.fire({
+    icon: "success",
+    title: "Congratulations",
+    text:  "Your ID Number is 12345",
+   button: "OK",
+  }).then(function () {
+      // Redirect the user
+      window.location.href = "/";
+    });
 }
+
+// function Random() {
+//   var randomNumber = Math.floor((Math.random() * 10) + 1);
+//  return `Your ID Number is ${randomNumber}`;
+// }

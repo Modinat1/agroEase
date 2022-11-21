acba411bd835eab1cfa6a86c0f088d1cd8f43211
tiacode <nwacynti25@gmail.com>
2022-11-21 10:01:02 +0000

    Updated functionality

--- agroease-project/src/Context/user-context/UserType.js
+++ agroease-project/src/Context/user-context/UserType.js
@@ -6,3 +6,7 @@ export const EDIT_USER = "EDIT_USER"
 export const DELETE_USER = "DELETE_USER"
 
 export const SUSPEND_USER = "SUSPEND_USER"
+
+export const SIGNUP_USER_SUCCESS = "SIGNUP_USER_SUCCESS"
+
+export const SIGNUP_USER_ERROR = "SIGNUP_USER_ERROR"

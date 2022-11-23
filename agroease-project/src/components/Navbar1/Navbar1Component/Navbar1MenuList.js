7b8d9d021de11df29f3c2a304698f7a0b3ffa224
Emsun <emsunconcept@gmail.com>
2022-11-23 14:34:52 +0100

    fix:deleted the buyer card routing

--- agroease-project/src/components/Navbar1/Navbar1Component/Navbar1MenuList.js
+++ agroease-project/src/components/Navbar1/Navbar1Component/Navbar1MenuList.js
@@ -1,52 +1,51 @@
-import React, { useState, useRef } from 'react'
-import { Link } from 'react-router-dom'
-import DropDown from './DropDown'
-import "./NavbarCompo.css"
-import {navItems} from "./NavbarListModal"
-import {GiHamburgerMenu} from "react-icons/gi"
-import { AiOutlineClose } from 'react-icons/ai';
-import "../Navbar1.css"
-
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import DropDown from "./DropDown";
+import "./NavbarCompo.css";
+import { navItems } from "./NavbarListModal";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
+import "../Navbar1.css";
 
 const Navbar1MenuList = () => {
-  const navRef = useRef();
-  const [dropDowns, setDropDowns] = useState(false)
-  
-  const showNavbar =()=> {
-    navRef.current.classList.toggle("responsive-nav")
-  }
+	const navRef = useRef();
+	const [dropDowns, setDropDowns] = useState(false);
 
-  return (
-    <div>
-        <ul className='navbar-menulist' ref={navRef}>
-           {
-            navItems.map((item) => {
+	const showNavbar = () => {
+		navRef.current.classList.toggle("responsive-nav");
+	};
 
-              if (item.title === "Account") {
-                return (
-                  <li key={item.id} onMouseEnter={()=> setDropDowns(true)} onMouseLeave={()=> setDropDowns(false)}>
-                    <Link to={item.path} >{item.title}</Link>
-                    {dropDowns && <DropDown />}
-                  </li>
-                )
-                
-              }
-              return (
-              <li key={item.id}>
-                <Link to={item.path}>{item.title}</Link>
-              </li>
-              )
-            })
-           }
-        <button>
-          <AiOutlineClose className="landing-nav-btn nav-close-btn" onClick={showNavbar}/>
-        </button>
-        </ul>
-        <button>
-          <GiHamburgerMenu className="landing-nav-btn" onClick={showNavbar}/>
-        </button>
-    </div>
-  )
-}
+	return (
+		<div>
+			<ul className='navbar-menulist' ref={navRef}>
+				{navItems.map((item) => {
+					// if (item.title === "Account") {
+					//   return (
+					//     <li key={item.id} onMouseEnter={()=> setDropDowns(true)} onMouseLeave={()=> setDropDowns(false)}>
+					//       <Link to={item.path} >{item.title}</Link>
+					//       {dropDowns && <DropDown />}
+					//     </li>
+					//   )
 
-export default Navbar1MenuList
+					// }
+					return (
+						<li key={item.id}>
+							<Link to={item.path}>{item.title}</Link>
+						</li>
+					);
+				})}
+				<button>
+					<AiOutlineClose
+						className='landing-nav-btn nav-close-btn'
+						onClick={showNavbar}
+					/>
+				</button>
+			</ul>
+			<button>
+				<GiHamburgerMenu className='landing-nav-btn' onClick={showNavbar} />
+			</button>
+		</div>
+	);
+};
+
+export default Navbar1MenuList;
--- agroease-project/src/components/UsersSignUp/UsersSignUpForm/UsersSignUpForm.jsx
+++ agroease-project/src/components/UsersSignUp/UsersSignUpForm/UsersSignUpForm.jsx
@@ -1,137 +1,154 @@
-import React from 'react'
-import { useFormik } from 'formik';
-import './UsersSignUpForm.css'
+import React from "react";
+import { useFormik } from "formik";
+import "./UsersSignUpForm.css";
 
 export const UsersSignUpForm = () => {
-    const validate = values => {
-   const errors = {};
-   if (!values.firstName) {
-     errors.firstName = 'Required';
-   } else if (values.firstName.length > 15) {
-     errors.firstName = 'Must be 15 characters or less';
-   }
- 
-   if (!values.lastName) {
-     errors.lastName = 'Required';
-   } else if (values.lastName.length > 20) {
-     errors.lastName = 'Must be 20 characters or less';
-   }
- 
-   if (!values.email) {
-     errors.email = 'Required';
-   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-     errors.email = 'Invalid email address';
-   }
+	const validate = (values) => {
+		const errors = {};
+		if (!values.firstName) {
+			errors.firstName = "Required";
+		} else if (values.firstName.length > 15) {
+			errors.firstName = "Must be 15 characters or less";
+		}
 
-   if (!values.password) {
-     errors.password = 'Required';
-   } else if (values.password.length < 8) {
-     errors.password = 'Must be 8 characters or more';
-   }
+		if (!values.lastName) {
+			errors.lastName = "Required";
+		} else if (values.lastName.length > 20) {
+			errors.lastName = "Must be 20 characters or less";
+		}
 
-   if (!values.confirmPassword) {
-     errors.confirmPassword = 'Required';
-   } else if (values.password !== values.confirmPassword) {
-     errors.confirmPassword = 'Must match with password';
-   }
- 
-   return errors;
- };
+		if (!values.email) {
+			errors.email = "Required";
+		} else if (
+			!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+		) {
+			errors.email = "Invalid email address";
+		}
 
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-      lastName: '',
-      email: '',
-      phoneNumber: '',
-      password: '',
-      confirmPassword: '',
-    },
-    validate,
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  return (
-    <React.Fragment>
-      
-    <div className='broker_signup_form'>
-    <form onSubmit={formik.handleSubmit}>
-     <h2 className='broker_signup_form_title'>Create an Account</h2>
-      <label htmlFor="firstName">First Name</label>
-      <input
-        id="firstName"
-        name="firstName"
-        type="text"
-        placeholder='First Name*'
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.firstName}
-      />
-      {formik.touched.firstName && formik.errors.firstName ? <div style={{"color" : "red"}}>{formik.errors.firstName}</div> : null}
+		if (!values.password) {
+			errors.password = "Required";
+		} else if (values.password.length < 8) {
+			errors.password = "Must be 8 characters or more";
+		}
 
-      <label htmlFor="lastName">Last Name</label>
-      <input
-        id="lastName"
-        name="lastName"
-        type="text"
-        placeholder='Last Name*'
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.lastName}
-      />
-      {formik.touched.lastName && formik.errors.lastName ? <div style={{"color" : "red"}}>{formik.errors.lastName}</div> : null}
+		if (!values.confirmPassword) {
+			errors.confirmPassword = "Required";
+		} else if (values.password !== values.confirmPassword) {
+			errors.confirmPassword = "Must match with password";
+		}
 
-      <label htmlFor="email">Email Address</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        placeholder='Email*'
-        onChange={formik.handleChange}
-        onBlur={formik.handleBlur}
-        value={formik.values.email}
-      />
-      {formik.touched.email && formik.errors.email ? <div style={{"color" : "red"}}>{formik.errors.email}</div> : null}
+		return errors;
+	};
 
-      <label htmlFor="phoneNumber">Phone Number</label>
-      <input
-        id="phoneNumber"
-        name="phoneNumber"
-        type="number"
-        placeholder='Phone Number*'
-        onBlur={formik.handleBlur}
-        value={formik.values.phoneNumber}
-      />
-      {/* {formik.errors.phoneNumber ? <div>{formik.errors.phoneNumber}</div> : null} */}
+	const formik = useFormik({
+		initialValues: {
+			firstName: "",
+			lastName: "",
+			email: "",
+			phoneNumber: "",
+			password: "",
+			confirmPassword: "",
+		},
+		validate,
+		onSubmit: (values) => {
+			alert(JSON.stringify(values, null, 2));
+		},
+	});
+	return (
+		<React.Fragment>
+			<div className='broker_signup_form'>
+				<form onSubmit={formik.handleSubmit}>
+					<h2 className='broker_signup_form_title'>Create an Account</h2>
+					<label htmlFor='firstName'>First Name</label>
+					<input
+						id='firstName'
+						name='firstName'
+						type='text'
+						placeholder='First Name*'
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.firstName}
+					/>
+					{formik.touched.firstName && formik.errors.firstName ? (
+						<div style={{ color: "red" }}>{formik.errors.firstName}</div>
+					) : null}
 
-      <label htmlFor="password">Password</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        placeholder='Password*'
-        onBlur={formik.handleBlur}
-        value={formik.values.password}
-      />
-      {formik.touched.password && formik.errors.password ? <div style={{"color" : "red"}}>{formik.errors.password}</div> : null}
+					<label htmlFor='lastName'>Last Name</label>
+					<input
+						id='lastName'
+						name='lastName'
+						type='text'
+						placeholder='Last Name*'
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.lastName}
+					/>
+					{formik.touched.lastName && formik.errors.lastName ? (
+						<div style={{ color: "red" }}>{formik.errors.lastName}</div>
+					) : null}
 
-      <label htmlFor="confirmPassword">Confirm Password</label>
-      <input
-       id="confirmPassword"
-        name="confirmPassword"
-        type="password"
-        placeholder='Confirm Password*'
-        onBlur={formik.handleBlur}
-        value={formik.values.confirmPassword}
-      />
-      {formik.touched.confirmPassword && formik.errors.confirmPassword ? <div style={{"color" : "red"}}>{formik.errors.confirmPassword}</div> : null}
+					<label htmlFor='email'>Email Address</label>
+					<input
+						id='email'
+						name='email'
+						type='email'
+						placeholder='Email*'
+						onChange={formik.handleChange}
+						onBlur={formik.handleBlur}
+						value={formik.values.email}
+					/>
+					{formik.touched.email && formik.errors.email ? (
+						<div style={{ color: "red" }}>{formik.errors.email}</div>
+					) : null}
 
-      <small className='broker_terms'>By clicking, you agree to the AgroEase <span className='broker_green'>Term of Service</span> and acknowledge, that AgroEase <span className='broker_green'>Privacy Policy</span> applies to you</small>
-      <button className='signup_form_btn' type="submit">Sign Up</button>
-    </form>
-    </div>
-    
-    </React.Fragment>
-  )
-}
+					<label htmlFor='phoneNumber'>Phone Number</label>
+					<input
+						id='phoneNumber'
+						name='phoneNumber'
+						type='text'
+						placeholder='Phone Number*'
+						onBlur={formik.handleBlur}
+						value={formik.values.phoneNumber}
+					/>
+					{/* {formik.errors.phoneNumber ? <div>{formik.errors.phoneNumber}</div> : null} */}
+
+					<label htmlFor='password'>Password</label>
+					<input
+						id='password'
+						name='password'
+						type='password'
+						placeholder='Password*'
+						onBlur={formik.handleBlur}
+						value={formik.values.password}
+					/>
+					{formik.touched.password && formik.errors.password ? (
+						<div style={{ color: "red" }}>{formik.errors.password}</div>
+					) : null}
+
+					<label htmlFor='confirmPassword'>Confirm Password</label>
+					<input
+						id='confirmPassword'
+						name='confirmPassword'
+						type='password'
+						placeholder='Confirm Password*'
+						onBlur={formik.handleBlur}
+						value={formik.values.confirmPassword}
+					/>
+					{formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+						<div style={{ color: "red" }}>{formik.errors.confirmPassword}</div>
+					) : null}
+
+					<small className='broker_terms'>
+						By clicking, you agree to the AgroEase{" "}
+						<span className='broker_green'>Term of Service</span> and
+						acknowledge, that AgroEase{" "}
+						<span className='broker_green'>Privacy Policy</span> applies to you
+					</small>
+					<button className='signup_form_btn' type='submit'>
+						Sign Up
+					</button>
+				</form>
+			</div>
+		</React.Fragment>
+	);
+};

1b156e15cbfd730395ac998b3db1202b66610b3b
sirconfy <csirconfy@gmail.com>
2022-11-28 09:07:15 +0100

    reset password in progress

--- agroease-project/src/components/ResetPassword/ResetPassword.jsx
+++ agroease-project/src/components/ResetPassword/ResetPassword.jsx
@@ -9,7 +9,7 @@ import Navbar1 from '../Navbar1/Navbar1'
 
 
 export const ResetPassword = () => {
-  // const navigate = useNavigate();
+  //const navigate = useNavigate();
   const notifySuccess = () => {
     toast.success("Reset Link has been sent to your email!", {
       position: toast.POSITION.TOP_CENTER,
@@ -22,44 +22,45 @@ export const ResetPassword = () => {
   };
   return (
 		<>
+            <Formik 
+                 initialValues={{
+									email: "",
+								}}
+								// validationSchema={SignupSchema}
+								onSubmit={async (values, { setSubmitting }) => {
+									const { email } = values;
+									setSubmitting(true);
+									try {
+										await axios.post(
+											"https://agro-ease-tiidelab.herokuapp.com/v1/auth/forgot-password",
+											{
+												email,
+											}
+										);
+										notifySuccess();
+										// navigate("/check-mail");
+									} catch (error) {
+										notifyError();
+									}
+								}}
+							>
+								{({
+									values,
+									errors,
+									touched,
+									handleChange,
+									handleSubmit,
+									isSubmitting,
+									/* and other goodies */
+								}) => (        
+	
+		<form >
+					
 		<Navbar1/>
+	
 				 <div className='reset_password_form'>
 					<h2 className='reset_password_form_title'>Reset Password</h2>
 						<label >Email Address</label>
-  
-            <Formik 
-              initialValues={{
-                email: "",
-              }}
-              // validationSchema={SignupSchema}
-              onSubmit= {async(values, { setSubmitting }) => {
-                const { email } = values;
-                setSubmitting(true);
-                try {
-                 await axios.post(
-                    "https://agro-ease-tiidelab.herokuapp.com/v1/auth/forgot-password",
-                    {
-                      email,
-                    }
-                  );
-                  notifySuccess();
-                  // navigate("/check-mail");
-                } catch (error) {
-                  notifyError();
-                }
-              }}
-            >
-              {({
-                values,
-                errors,
-                touched,
-                handleChange,
-                handleSubmit,
-                isSubmitting,
-                /* and other goodies */
-              }) => (        
-	
-		<form >
 	 					<input
 								type="email"
 								name="email"
@@ -67,20 +68,17 @@ export const ResetPassword = () => {
 								onChange={handleChange}
 								value={values.email}
 								/>
-								<div className='errors'>{errors.email && touched.email && errors.email}</div>
+								{/* <div className='errors'>{errors.email && touched.email && errors.email}</div> */}
 								<button className='reset_btn' type="submit"  onClick={handleSubmit}
                       disabled={isSubmitting}>
 									     {isSubmitting ? "loading..." : "continue"}
 								</button>
+								</div>
 							</form>
 							)}
 							  </Formik>
-									<ToastContainer />
-           
-							</div>
-						
-							
-							</>
-							
+								
+									<ToastContainer />							
+							</>							
   );
 }

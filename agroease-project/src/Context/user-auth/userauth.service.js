22fed4a7ea060513af3c2ef5594243587cf156f2
tiacode <nwacynti25@gmail.com>
2022-11-29 08:56:29 +0000

    Worked on the authentication

--- agroease-project/src/Context/user-auth/userauth.service.js
+++ agroease-project/src/Context/user-auth/userauth.service.js
@@ -9,21 +9,7 @@ import { UserAuthState } from "./UserAuthState"
 //Sign up new users
 const signUpNewUser = async (values, Autherror) => {
     
-    try {
-        const response = await axiosInstance.post("v1/auth/register", values)
-        console.log(response.data)
-        return response.data
-    }
-
-    catch (error) {
-        if(error.response) {
-        //     console.log(error)
-        //     console.log(error.response.status)
-		// 	console.log(error.response.data.message)
-			
-        return error.response.status
-    }
-    }
+   
 }
 
 const AuthServices = {

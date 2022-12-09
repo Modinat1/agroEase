1d627bfa4a1c825a11f8092e3917b6045c325f41
Emsun <emsunconcept@gmail.com>
2022-12-09 13:06:29 +0100

    Rendered the items on the homepage

--- agroease-project/src/pages/Home/Categories.jsx
+++ agroease-project/src/pages/Home/Categories.jsx
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axiosInstance from "../../Context/axios-config/axios-user-config";
 import { ProductContext } from "../../Context/Store/productContext";
 
 export const Categories = () => {
 	const productStore = React.useContext(ProductContext);
+	// const [categories, setcategories] = useState([]);
+
+	// const getCategories = async () => {
+	// 	const categories = await axiosInstance.get("v1/category");
+	// 	console.log(categories.data);
+	// 	setcategories(categories.data.name);
+	// };
+
+	// useEffect(() => {
+	// 	getCategories();
+	// }, []);
+
+	const [allProducts, setallProducts] = useState([]);
+
+	const getVerifiedProducts = async () => {
+		try {
+			const verifiedProducts = await axiosInstance.get("/v1/product/");
+			console.log(verifiedProducts);
+			setallProducts(verifiedProducts.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getVerifiedProducts();
+	}, []);
 
 	const categories = [
 		{
 			title: "Livestocks",
-			action: "livestocks",
+			action: "Livestocks",
+			body: [],
 		},
 		{
 			title: "Crops",
 			action: "Crops",
+			body: [],
 		},
 		{
 			title: "Farm Inputs",
 			action: "Farm Inputs",
+			body: [],
 		},
 		{
 			title: "Processed Products",
 			action: "Processed Products",
+			body: [],
 		},
 		{
 			title: "Equipments",
 			action: "Equipments",
+			body: [],
 		},
 	];
 

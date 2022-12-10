f61a09a1ba0b4323e08593bfe49fa2a1dae57895
tiacode <nwacynti25@gmail.com>
2022-12-10 04:53:25 +0000

    Worked on the logout function aand updated the buyer's dashboard

--- agroease-project/src/users/Buyer/BuyerDashboard/BuyerDashboardPage.jsx
+++ agroease-project/src/users/Buyer/BuyerDashboard/BuyerDashboardPage.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-//import BrokerDashnav from "../../../components/dashboard-nav-and-side/BrokerDashnav";
-// import BrokerSidebar from "../../../components/dashboard-nav-and-side/BrokerSidebar";
-// import BuyerSidebar from "../../../components/dashboard-nav-and-side/BuyerSidebar";
 import NewBrokerNavbar from "../../../components/dashboard-nav-and-side/NewBrokerNavbar";
-// import NewBrokerSidebar from "../../../components/dashboard-nav-and-side/NewBrokerSidebar";
 import NewBuySidebar from "../../../components/dashboard-nav-and-side/NewBuySidebar";
-// import Dashnav from "../../../components/dashboard-nav-and-side/Dashnav";
-// import Sidebar from "../../../components/dashboard-nav-and-side/Sidebar";
-// import { Sidebar } from "../Sidebar";
-// import { NavBar2 } from "../NavBar2";
+import NewHeroSection from "../../../pages/NewWork/NewHeroSection/NewHeroSection";
 import "./BuyerDashboard.css";
 import { BuyerDashCard } from "./BuyerDashCard";
 
@@ -26,6 +19,7 @@ export const BuyerDashboardPage = () => {
 					</section>
 
 					<BuyerDashCard />
+					<NewHeroSection />
 				</section>
 			</div>
 		</>
